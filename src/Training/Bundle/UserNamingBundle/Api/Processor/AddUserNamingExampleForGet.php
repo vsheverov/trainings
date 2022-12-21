15b802a03105200e1540d4c8321418dfb08e8987
Vitalii Sheverov <vsheverov@oroinc.com>
2022-12-21 11:14:59 +0100

    INTAP-1718: API - divide one service API Processor to two

--- src/Training/Bundle/UserNamingBundle/Api/Processor/AddUserNamingExampleForGet.php
+++ src/Training/Bundle/UserNamingBundle/Api/Processor/AddUserNamingExampleForGet.php
@@ -6,11 +6,12 @@ use Oro\Component\ChainProcessor\ContextInterface;
 use Oro\Component\ChainProcessor\ProcessorInterface;
 use Training\Bundle\UserNamingBundle\Formatter\UserNameFormatter;
 
-class AddUserNamingExampleForGetMethods implements ProcessorInterface
+class AddUserNamingExampleForGet implements ProcessorInterface
 {
     private const META_PROPERTY_KEY = 'nameExample';
 
     private UserNameFormatter $nameFormatter;
+
     public function __construct(UserNameFormatter $nameFormatter)
     {
         $this->nameFormatter = $nameFormatter;
@@ -18,33 +19,17 @@ class AddUserNamingExampleForGetMethods implements ProcessorInterface
 
     public function process(ContextInterface $context)
     {
-        if (! $context->getMetadata()?->hasProperty(self::META_PROPERTY_KEY)) {
+        if (!$context->getMetadata()?->hasProperty(self::META_PROPERTY_KEY)) {
             return;
         }
 
         $result = $context->getResult();
 
-        if (! $result) {
+        if (!$result || !isset($item['format'])) {
             return;
         }
 
-        $isSingleResult = $context->getAction() === 'get';
-
-        if ($isSingleResult) {
-            $result = [$result];
-        }
-
-        foreach ($result as $key => $item) {
-            if (!isset($item['format'])) {
-                continue;
-            }
-
-            $result[$key][self::META_PROPERTY_KEY] = $this->nameFormatter->formatExample($item['format']);
-        }
-
-        if ($isSingleResult) {
-            $result = $result[0];
-        }
+        $result[self::META_PROPERTY_KEY] = $this->nameFormatter->formatExample($item['format']);
 
         $context->setResult($result);
     }
---
+++ src/Training/Bundle/UserNamingBundle/Api/Processor/AddUserNamingExampleForGetList.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Training\Bundle\UserNamingBundle\Api\Processor;
+
+use Oro\Component\ChainProcessor\ContextInterface;
+use Oro\Component\ChainProcessor\ProcessorInterface;
+use Training\Bundle\UserNamingBundle\Formatter\UserNameFormatter;
+
+class AddUserNamingExampleForGetList implements ProcessorInterface
+{
+    private const META_PROPERTY_KEY = 'nameExample';
+
+    private UserNameFormatter $nameFormatter;
+    public function __construct(UserNameFormatter $nameFormatter)
+    {
+        $this->nameFormatter = $nameFormatter;
+    }
+
+    public function process(ContextInterface $context)
+    {
+        if (! $context->getMetadata()?->hasProperty(self::META_PROPERTY_KEY)) {
+            return;
+        }
+
+        $result = $context->getResult();
+
+        if (! $result || !is_array($result)) {
+            return;
+        }
+
+        foreach ($result as $key => $item) {
+            if (!isset($item['format'])) {
+                continue;
+            }
+
+            $result[$key][self::META_PROPERTY_KEY] = $this->nameFormatter->formatExample($item['format']);
+        }
+
+        $context->setResult($result);
+    }
+}
